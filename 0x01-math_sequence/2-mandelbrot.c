2725e3b55a4c0fd9c48bfe0ae2c0c05b96595830
Pierre Dureau <dureaupierre53@gmail.com>
2022-03-29 12:40:57 +0200

    Fix

--- 0x01-math_sequence/2-mandelbrot.c
+++ 0x01-math_sequence/2-mandelbrot.c
@@ -8,9 +8,9 @@
 void main(void)
 {
 	int x, y, i;
-	double a, b, t, n = 250;
+	double r, n = 250;
 	int width = n * 4, height = n * 4;
-	complex c;
+	complex c, t;
 	FILE *pgmimg;
 
 	pgmimg = fopen("mandelbrot.pgm", "wb");
@@ -20,18 +20,18 @@ void main(void)
 
 	for (x = 0; x < width; x++)
 	{
-		a = 2 - (x / n);
+		r = -2 + (x / n);
 		for (y = 0; y < height; y++)
 		{
-			b = -2 + (y / n);
-			c.re = 0;
-			c.im = 0;
+			c.re = r;
+			c.im = 2 - (y / n);
 			for (i = 1; i < 255; i++)
 			{
-				t = c.re;
-				c.re = (c.re * c.re) - (c.im * c.im) + b;
-				c.im = (2 * t * c.im) + a;
-				if ((c.re * c.re) + (c.im * c.im) > 4)
+				t.re = c.re;
+				t.im = c.im;
+				multiplication(c, c, &c);
+				addition(c, t, &c);
+				if (c.re > 2 || c.im > 2)
 					break;
 			}
 			if (i == 255)
