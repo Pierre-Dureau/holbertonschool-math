852768c23a1be732c44783548dd7b56b15d6ed70
Pierre Dureau <dureaupierre53@gmail.com>
2022-04-27 20:06:18 +0200

    Fix pycodestyle

--- 0x04-Math_Gauss_elimination/1-inverse.py
+++ 0x04-Math_Gauss_elimination/1-inverse.py
@@ -17,10 +17,11 @@ if __name__ == "__main__":
 
         return A1
 
-    A = np.zeros((3, 3))
+    A = np.zeros((4, 4))
     A[2, 0] = 1
     A[0, 1] = 1
-    A[1, 2] = 1
-    print("The matrix A:\n", A)
+    A[1, 3] = 1
+    A[3, 2] = 1
+    print("The matrix A:\n",A)
     print("The inverse of the matrix A:\n", inverse(A))
     print("The identity matrix (The matrix A x The inverse of the matrix A): \n", A @ inverse(A))
