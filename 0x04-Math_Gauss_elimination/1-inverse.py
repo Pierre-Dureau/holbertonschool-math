c396f2ce1b14b0daef6e92eea7c0545f08004877
Pierre Dureau <dureaupierre53@gmail.com>
2022-04-27 19:14:38 +0200

    Return the inverse of a matrix

--- 0x04-Math_Gauss_elimination/1-inverse.py
+++ 0x04-Math_Gauss_elimination/1-inverse.py
@@ -6,14 +6,18 @@ gaussian_elimination = __import__('0-gaussian_elimination').gaussian_elimination
 if __name__ == "__main__":
 
     def inverse(A):
+        B = np.zeros(np.shape(A))
+        row, col = np.shape(B)
 
-        return np.linalg.inv(A)
+        i = 0
+        for i in range(0, col):
+            B[i][i] = 1
 
-    A = np.zeros((4, 4))
-    A[2, 0] = 1
-    A[0, 1] = 1
-    A[1, 3] = 1
-    A[3, 2] = 1
+        A1 = gaussian_elimination(A, B)
+
+        return A1
+
+    A = [[-3, 2, -1], [6, -6, 7], [3, -4, 4]]
     print("The matrix A:\n", A)
     print("The inverse of the matrix A:\n", inverse(A))
     print("The identity matrix (The matrix A x The inverse of the matrix A): \n", A @ inverse(A))
